3d8e85d28d5d917a75eb316b8827a927297d4e7a
cmazx <artem.mehmedov@getex.com>
2020-03-28 03:53:14 +0300

    Remove typed fillable, due error with typed field on server

--- app/User.php
+++ app/User.php
@@ -10,15 +10,6 @@ class User extends Authenticatable
     use Notifiable;
 
     /**
-     * The attributes that are mass assignable.
-     *
-     * @var array
-     */
-    protected array $fillable = [
-        'email', 'password',
-    ];
-
-    /**
      * The attributes that should be hidden for arrays.
      *
      * @var array
